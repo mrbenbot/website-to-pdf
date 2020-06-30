8867ac71aed948ee98ba1a85af9b8e207c3e5069
ben <benlee.thatsme@gmail.com>
2020-06-30 17:53:25 +0100

    pdf'

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const express = require("express")
-const { getPdf } = require("./pdf")
+const { getPdf, getCode } = require("./pdf")
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
-
-
-app.get("/file", async (req, res) => {
+app.get("/pdf", async (req, res) => {
     const { url } = req.query
     const pdf = await getPdf(url)
     res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length })
     res.send(pdf)
 })
-
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
