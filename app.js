291d6c2a3a100933c6457397f1d0b8f9fe6c42be
ben <benlee.thatsme@gmail.com>
2020-06-30 18:03:47 +0100

    heroku opts for puppeteer

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require("express")
-const { getPdf, getCode } = require("./pdf")
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const { getPdf } = require("./pdf");
+const PORT = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
 app.get("/pdf", async (req, res) => {
-    const { url } = req.query
-    const pdf = await getPdf(url)
-    res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length })
-    res.send(pdf)
-})
+  const { url } = req.query;
+  const pdf = await getPdf(url);
+  res.set({ "Content-Type": "application/pdf", "Content-Length": pdf.length });
+  res.send(pdf);
+});
 
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-})
+  console.log(`listening on port ${PORT}`);
+});
--- pdf.js
+++ pdf.js
@@ -1,12 +1,15 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require("puppeteer");
 
 module.exports = {
-    getPdf: async (url) => {
-        const browser = await puppeteer.launch({ headless: true });
-        const page = await browser.newPage();
-        await page.goto(url, { waitUntil: 'networkidle0' });
-        const pdf = await page.pdf({ format: 'A4' });
-        await browser.close();
-        return pdf
-    }
-}
+  getPdf: async (url) => {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox"],
+    });
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: "networkidle0" });
+    const pdf = await page.pdf({ format: "A4" });
+    await browser.close();
+    return pdf;
+  },
+};
